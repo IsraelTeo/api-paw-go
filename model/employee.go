0dfdc08d9d2e0ea21583e1e3603cc32a4c05c037
Israel <israel.bastion@gmail.com>
2024-11-30 11:53:20 -0500

    number debug

--- model/employee.go
+++ model/employee.go
@@ -12,7 +12,7 @@ type Employee struct {
 	LastName    string       `json:"last_name" gorm:"size:70;not null" validate:"required,min=3,max=90"`
 	DNI         string       `json:"dni" gorm:"size:15;unique;not null" validate:"required,max=70"`
 	Email       string       `json:"email" gorm:"size:100;unique;not null" validate:"required,email"`
-	PhoneNumber string       `json:"phone_number" gorm:"unique;size:15" validate:"required,len=15,numeric"`
+	PhoneNumber string       `json:"phone_number" gorm:"unique;size:15" validate:"required,max=15,numeric"`
 	Direction   string       `json:"direction" gorm:"size:100" validate:"required,max=100"`
 	BirthDate   time.Time    `json:"birth_date" gorm:"not null" validate:"required,datetime=2006-01-02"`
 	TypeID      uint         `json:"type_id" gorm:"not null;index" validate:"required"`
